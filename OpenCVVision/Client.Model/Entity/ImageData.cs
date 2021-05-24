a388bc8b592776ce90c2c498907e5702cad43787
lolo <lolo123777@outlook.com>
2021-05-24 14:29:51 +0800

    添加图像数据管理服务，负责操作的原始数据保存； 暂时在内存中保存数据； 后续考虑磁盘的缓存优化

---
+++ OpenCVVision/Client.Model/Entity/ImageData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenCvSharp;
+
+namespace Client.Model.Entity
+{
+    public class ImageData
+    {
+        public Guid ImageId { get; set; }
+        public Mat ImageMat { get; set; }
+        public string TxtMarker { get; set; }
+    }
+}
---
+++ OpenCVVision/Client.Model/Service/IImageDataManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Client.Model.Entity;
+
+using DynamicData;
+
+using OpenCvSharp;
+
+namespace Client.Model.Service
+{
+    public interface IImageDataManager
+    {
+        SourceCache<ImageData, Guid> SourceCacheImageData { set; get; }
+
+        bool AddImage(string imageMarkTxt, Mat mat);
+
+        ImageData GetImage(Guid guid);
+
+        bool RemoveIamge(string imageMarkTxt);
+
+        bool RemoveImage(Guid guid);
+    }
+}
