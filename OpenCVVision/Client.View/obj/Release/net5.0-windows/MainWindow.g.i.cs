a4a092f0f0dd14296de4af60f500a0f0533461b9
洛洛 <lolo123777@outlook.com>
2021-06-04 12:19:15 +0800

    清理
    Former-commit-id: 3b93322f1ac2af446d49743d09cb4040ab30309f

--- OpenCVVision/Client.View/obj/Release/net5.0-windows/MainWindow.g.i.cs
+++ OpenCVVision/Client.View/obj/Release/net5.0-windows/MainWindow.g.i.cs
@@ -10,6 +10,8 @@
 //------------------------------------------------------------------------------
 
 using Client;
+using Client.View;
+using Client.ViewModel;
 using MahApps.Metro;
 using MahApps.Metro.Accessibility;
 using MahApps.Metro.Actions;
@@ -48,6 +50,7 @@ using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Shell;
+using Wpf.Controls.PanAndZoom;
 
 
 namespace Client {
@@ -110,7 +113,7 @@ namespace Client {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Client.View;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Client.View;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
